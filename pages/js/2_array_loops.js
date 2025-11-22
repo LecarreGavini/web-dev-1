d3ab7e206140558580f622a5d890ec7d16a4fd5d
Lecarre Gavini <lecarregavini@gmail.com>
2025-11-22 10:13:27 +0800

    add arr and loops

--- pages/js/2_array_loops.js
+++ pages/js/2_array_loops.js
@@ -1,13 +1,57 @@
+// arrays
+const brands = ['dell', 'hp', 'lenovo']
+
 // Loops
 //   For
 //   While
 //   For of
 
-function introduce() {
-	for (let i = 0; i < attributes.length; i++) {
-		console.log(`I am ${attributes[i]}`)
+function forLoop() {
+	// pattern: for (initialize, condition, increment)
+	for (let i = 0; i < brands.length; i++) {
+		console.log(`I am ${brands[i]}`)
+	}
+	// let i = 0
+	// for (; i < brands.length; ) {
+	// 	// i = i + 1
+	// 	// i += 1
+	// 	// i++
+	// 	console.log(`I am ${brands[i]}`)
+	// }
+	// for (let i = brands.length - 1; i >= 0; i--) {
+	// 	console.log(`I am ${brands[i]}`)
+	// }
+}
+
+const whileLoop = () => {
+	let i = 0
+	while (i < brands.length) {
+		console.log(`I am ${brands[i]}`)
+		i++
 	}
 }
 
-// arrays
-const brands = ['dell', 'hp', 'lenovo']
+const forOfLoop = () => {
+	// pattern: for (newVariable of array)
+	for (const brand of brands) {
+		console.log(`I am ${brand}`)
+	}
+}
+
+const forEachLoop = () => {
+	brands.forEach(brand => {
+		console.log(`I am ${brand}`)
+	})
+}
+
+const mapLoop = () => {
+	return brands.map(brand => {
+		if (brand == 'hp') {
+			return `${brand} - 2.0`
+		} else if (brand == 'dell') {
+			return `${brand} - v2`
+		} else {
+			return `${brand} - outdated`
+		}
+	})
+}
